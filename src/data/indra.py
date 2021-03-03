5debd39b9ef303985ec58a582bd4fd2291e06d54
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2021-03-03 11:25:00 +0100

    ADd indra exporter

--- src/data/indra.py
+++ src/data/indra.py
@@ -8,11 +8,62 @@ python -m src.data.indra
 
 import logging
 
+import pandas as pd
 import pybel
-from pybel.constants import ANNOTATIONS
+from pybel.constants import ANNOTATIONS, EVIDENCE, RELATION, CITATION
+
 from ..constants import DUMMY_EXAMPLE_INDRA
 
 logger = logging.getLogger(__name__)
+
+
+def create_context_subgraph(graph: pybel.BELGraph, context_annotation: str) -> pybel.BELGraph:
+    """Create a subgraph based on context annotations."""
+    subgraph = graph.child()
+    subgraph.name = f'INDRA graph contextualized for {context_annotation}'
+
+    # Iterate through the graph and infer a subgraph with edges that contain the annotation of interest
+    for u, v, k, data in graph.edges(data=True, keys=True):
+        if ANNOTATIONS in data and context_annotation in data[ANNOTATIONS]:
+            subgraph.add_edge(u, v, k, **data)
+
+    string = f'Number of nodes/edges in the inferred subgraph "{context_annotation}": \
+    {subgraph.number_of_nodes()} {subgraph.number_of_edges()}'
+
+    logger.info(string)
+
+    return subgraph
+
+
+def dump_edgelist(graph: pybel.BELGraph, name: str) -> None:
+    """Dump tsv file for ml purposes."""
+    triples = []
+
+    # Iterate through the graph and infer a subgraph with edges that contain the annotation of interest
+    for u, v, data in graph.edges(data=True):
+
+        if not data[EVIDENCE]:
+            logger.warning(f'not evidence found in {data}')
+            continue
+
+        if len(data[ANNOTATIONS][name]) > 1:
+            logger.warning(f'triple has more than one label -> {data[ANNOTATIONS][name]}')
+            continue
+
+        for label_annotation in data[ANNOTATIONS][name]:
+            triples.append(
+                {
+                    'source': u,
+                    'relation': data[RELATION],
+                    'target': v,
+                    'evidence': data[EVIDENCE],
+                    'pmid': data[CITATION],
+                    'class': label_annotation,
+                },
+            )
+
+    df = pd.DataFrame(triples)
+    df.to_csv(f'{name}.tsv', sep='\t', index=False)
 
 
 def read_indra_triples(
@@ -23,26 +74,29 @@ def read_indra_triples(
 
     logger.info(indra_kg.summarize())
 
-    context_annotations = set()
+    # Print all the annotations in the graph
+    all_annotations = set()
 
     for _, _, data in indra_kg.edges(data=True):
         if ANNOTATIONS in data:
             for key in data[ANNOTATIONS]:
-                context_annotations.add(key)
-                
-                # TODO: check for triples without any evidence
+                all_annotations.add(key)
 
-    #TODO: "context" annotation is missing
-    logger.info(context_annotations)
-    
-    
-    # TODO: get a subgraph for each of the four annnotations of interest
-    
-    # TODO: summarize each of the subgraphs
-    
-    # TODO: export them in edgelist with a fourth column with the label/class (e.g., human, rat...)
-    
-    # TODO: TSV FILE |  source, target, relation, evidence, label
+    logger.info(all_annotations)
+
+    organ_subgraph = create_context_subgraph(indra_kg, 'organ')
+    species_subgraph = create_context_subgraph(indra_kg, 'species')
+    disease_subgraph = create_context_subgraph(indra_kg, 'disease')
+    cell_type_subgraph = create_context_subgraph(indra_kg, 'cell_type')
+    cell_line_subgraph = create_context_subgraph(indra_kg, 'cell_line')
+    location_subgraph = create_context_subgraph(indra_kg, 'location')
+
+    dump_edgelist(organ_subgraph, 'organ')
+    dump_edgelist(species_subgraph, 'species')
+    dump_edgelist(disease_subgraph, 'disease')
+    dump_edgelist(cell_type_subgraph, 'cell_type')
+    dump_edgelist(cell_line_subgraph, 'cell_line')
+    dump_edgelist(location_subgraph, 'location')
 
 
 if __name__ == '__main__':
