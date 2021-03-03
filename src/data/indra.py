fc3233e890c9dbecff9bd49d0c61a9950afba036
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2021-03-03 13:39:40 +0100

    Add summary

--- src/data/indra.py
+++ src/data/indra.py
@@ -8,7 +8,7 @@ python -m src.data.indra
 
 import logging
 import os
-from typing import List
+from typing import Any, Dict, List
 
 import pandas as pd
 import pybel
@@ -40,9 +40,13 @@ def create_context_subgraph(graph: pybel.BELGraph, context_annotations: List[str
     return subgraph
 
 
-def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> None:
+def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> Dict[str, Any]:
     """Dump tsv file for ml purposes."""
     triples = []
+
+    summary = {
+        'context': name,
+    }
 
     # Iterate through the graph and infer a subgraph with edges that contain the annotation of interest
     for u, v, data in graph.edges(data=True):
@@ -76,10 +80,35 @@ def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> N
 
     df = pd.DataFrame(triples)
 
-    # TODO: filter classes that are really small
-    logger.info(df['class'].value_counts())
+    """Removing labels that appear less than X% from the total"""
+    label_counts = df['class'].value_counts().to_dict()
+    # 0.05%
+    percentage = 0.05
+    cutoff = int(df.shape[0] * percentage)
+
+    labels_to_remove = {
+        label: count
+        for label, count in label_counts.items()
+        if count < cutoff
+    }
+
+    logger.warning(f'labels removed due to low occurrence {labels_to_remove}')
+
+    logger.info(f'raw triples {df.shape[0]}')
+    df = df[df['class'].isin(list(labels_to_remove.keys()))]
+
+    logger.info(f'triples after filtering {df.shape[0]}')
+
+    final_labels = df["class"].unique()
+    logger.info(f' final number of classes {len(final_labels)}')
+
+    summary['number_of_triples'] = df.shape[0]
+    summary['number_of_labels'] = len(final_labels)
+    summary['labels'] = df['class'].value_counts().to_dict()
 
     df.to_csv(os.path.join(DATA_DIR, f'{name}.tsv'), sep='\t', index=False)
+
+    return summary
 
 
 def read_indra_triples(
@@ -107,12 +136,23 @@ def read_indra_triples(
     cell_line_subgraph = create_context_subgraph(indra_kg, ['CellLine'])
     location_subgraph = create_context_subgraph(indra_kg, ['CellStructure'])
 
-    dump_edgelist(organ_subgraph, ['MeSHAnatomy'], 'organ')
-    dump_edgelist(species_subgraph, ['TAX_ID'], 'species')
-    dump_edgelist(disease_subgraph, ['MeSHDisease', 'Disease'], 'disease')
-    dump_edgelist(cell_type_subgraph, ['Cell'], 'cell_type')
-    dump_edgelist(cell_line_subgraph, ['CellLine'], 'cell_line')
-    dump_edgelist(location_subgraph, ['CellStructure'], 'location')
+    organ_summary = dump_edgelist(organ_subgraph, ['MeSHAnatomy'], 'organ')
+    species_summary = dump_edgelist(species_subgraph, ['TAX_ID'], 'species')
+    disease_summary = dump_edgelist(disease_subgraph, ['MeSHDisease', 'Disease'], 'disease')
+    cell_type_summary = dump_edgelist(cell_type_subgraph, ['Cell'], 'cell_type')
+    cell_line_summary = dump_edgelist(cell_line_subgraph, ['CellLine'], 'cell_line')
+    location_summary = dump_edgelist(location_subgraph, ['CellStructure'], 'location')
+
+    summary_df = pd.DataFrame([
+        organ_summary,
+        species_summary,
+        disease_summary,
+        cell_type_summary,
+        cell_line_summary,
+        location_summary,
+    ])
+
+    summary_df.to_csv(os.path.join(DATA_DIR, 'summary.tsv'), sep='\t', index=False)
 
 
 if __name__ == '__main__':
