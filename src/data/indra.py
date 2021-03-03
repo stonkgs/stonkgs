8abbeacf225dbd4bd18006ea1cec40e74ca77d8a
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2021-03-03 13:18:06 +0100

    Add code

--- src/data/indra.py
+++ src/data/indra.py
@@ -6,8 +6,9 @@ Run with:
 python -m src.data.indra
 """
 
-import os
 import logging
+import os
+from typing import List
 
 import pandas as pd
 import pybel
@@ -18,17 +19,20 @@ from ..constants import DATA_DIR, DUMMY_EXAMPLE_INDRA
 logger = logging.getLogger(__name__)
 
 
-def create_context_subgraph(graph: pybel.BELGraph, context_annotation: str) -> pybel.BELGraph:
+def create_context_subgraph(graph: pybel.BELGraph, context_annotations: List[str]) -> pybel.BELGraph:
     """Create a subgraph based on context annotations."""
     subgraph = graph.child()
-    subgraph.name = f'INDRA graph contextualized for {context_annotation}'
+    subgraph.name = f'INDRA graph contextualized for {context_annotations}'
 
     # Iterate through the graph and infer a subgraph with edges that contain the annotation of interest
     for u, v, k, data in graph.edges(data=True, keys=True):
-        if ANNOTATIONS in data and context_annotation in data[ANNOTATIONS]:
+        if ANNOTATIONS in data and any(
+            annotation in data[ANNOTATIONS]
+            for annotation in context_annotations
+        ):
             subgraph.add_edge(u, v, k, **data)
 
-    string = f'Number of nodes/edges in the inferred subgraph "{context_annotation}": \
+    string = f'Number of nodes/edges in the inferred subgraph "{context_annotations}": \
     {subgraph.number_of_nodes()} {subgraph.number_of_edges()}'
 
     logger.info(string)
@@ -36,7 +40,7 @@ def create_context_subgraph(graph: pybel.BELGraph, context_annotation: str) -> p
     return subgraph
 
 
-def dump_edgelist(graph: pybel.BELGraph, name: str) -> None:
+def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> None:
     """Dump tsv file for ml purposes."""
     triples = []
 
@@ -47,23 +51,33 @@ def dump_edgelist(graph: pybel.BELGraph, name: str) -> None:
             logger.warning(f'not evidence found in {data}')
             continue
 
-        if len(data[ANNOTATIONS][name]) > 1:
-            logger.warning(f'triple has more than one label -> {data[ANNOTATIONS][name]}')
-            continue
+        # Multiple annotations
+        for annotation in data[ANNOTATIONS]:
 
-        for label_annotation in data[ANNOTATIONS][name]:
-            triples.append(
-                {
-                    'source': u,
-                    'relation': data[RELATION],
-                    'target': v,
-                    'evidence': data[EVIDENCE],
-                    'pmid': data[CITATION],
-                    'class': label_annotation,
-                },
-            )
+            if annotation not in annotations:
+                continue
+
+            # Skip multiple classes in the triple for the same annotation
+            if len(data[ANNOTATIONS][annotation]) > 1:
+                logger.warning(f'triple has more than one label -> {data[ANNOTATIONS][annotation]}')
+                continue
+
+            for label_annotation in data[ANNOTATIONS][annotation]:
+                triples.append(
+                    {
+                        'source': u,
+                        'relation': data[RELATION],
+                        'target': v,
+                        'evidence': data[EVIDENCE],
+                        'pmid': data[CITATION],
+                        'class': label_annotation,
+                    },
+                )
 
     df = pd.DataFrame(triples)
+
+    logger.info(df['class'].value_counts())
+
     df.to_csv(os.path.join(DATA_DIR, f'{name}.tsv'), sep='\t', index=False)
 
 
@@ -85,19 +99,19 @@ def read_indra_triples(
 
     logger.info(all_annotations)
 
-    organ_subgraph = create_context_subgraph(indra_kg, 'organ')
-    species_subgraph = create_context_subgraph(indra_kg, 'species')
-    disease_subgraph = create_context_subgraph(indra_kg, 'disease')
-    cell_type_subgraph = create_context_subgraph(indra_kg, 'cell_type')
-    cell_line_subgraph = create_context_subgraph(indra_kg, 'cell_line')
-    location_subgraph = create_context_subgraph(indra_kg, 'location')
+    organ_subgraph = create_context_subgraph(indra_kg, ['MeSHAnatomy'])
+    species_subgraph = create_context_subgraph(indra_kg, ['TAX_ID'])
+    disease_subgraph = create_context_subgraph(indra_kg, ['MeSHDisease', 'Disease'])
+    cell_type_subgraph = create_context_subgraph(indra_kg, ['Cell'])
+    cell_line_subgraph = create_context_subgraph(indra_kg, ['CellLine'])
+    location_subgraph = create_context_subgraph(indra_kg, ['CellStructure'])
 
-    dump_edgelist(organ_subgraph, 'organ')
-    dump_edgelist(species_subgraph, 'species')
-    dump_edgelist(disease_subgraph, 'disease')
-    dump_edgelist(cell_type_subgraph, 'cell_type')
-    dump_edgelist(cell_line_subgraph, 'cell_line')
-    dump_edgelist(location_subgraph, 'location')
+    dump_edgelist(organ_subgraph, ['MeSHAnatomy'], 'organ')
+    dump_edgelist(species_subgraph, ['TAX_ID'], 'species')
+    dump_edgelist(disease_subgraph, ['MeSHDisease', 'Disease'], 'disease')
+    dump_edgelist(cell_type_subgraph, ['Cell'], 'cell_type')
+    dump_edgelist(cell_line_subgraph, ['CellLine'], 'cell_line')
+    dump_edgelist(location_subgraph, ['CellStructure'], 'location')
 
 
 if __name__ == '__main__':
