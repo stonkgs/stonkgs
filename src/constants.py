787e6f416d09ded70176a1e8d8ca3f28c2f51f4b
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2021-03-05 10:28:17 +0100

    Improve pipieline data

--- src/constants.py
+++ src/constants.py
@@ -9,13 +9,38 @@ SOURCE_DIR = os.path.join(os.path.abspath(os.path.join(HERE, os.pardir)))
 PROJECT_DIR = os.path.join(os.path.abspath(os.path.join(HERE, os.pardir)))
 
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
-OUTPUT_DIR = os.path.join(PROJECT_DIR, 'output')
+
+# Sub-directories of data
+RAW_DIR = os.path.join(DATA_DIR, 'raw')
+INPUT_DIR = os.path.join(DATA_DIR, 'input')
+OUTPUT_DIR = os.path.join(DATA_DIR, 'output')
+MISC_DIR = os.path.join(DATA_DIR, 'misc')
+
+# Directories for each annotation type
+ORGAN_DIR = os.path.join(INPUT_DIR, 'organ')
+DISEASE_DIR = os.path.join(INPUT_DIR, 'disease')
+LOCATION_DIR = os.path.join(INPUT_DIR, 'location')
+CELL_TYPE_DIR = os.path.join(INPUT_DIR, 'cell_type')
+CELL_LINE_DIR = os.path.join(INPUT_DIR, 'cell_line')
+SPECIES_DIR = os.path.join(INPUT_DIR, 'species')
 
 MODELS_DIR = os.path.join(PROJECT_DIR, 'models')
 
+os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(MODELS_DIR, exist_ok=True)
+os.makedirs(RAW_DIR, exist_ok=True)
+os.makedirs(INPUT_DIR, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+os.makedirs(MISC_DIR, exist_ok=True)
+
+os.makedirs(ORGAN_DIR, exist_ok=True)
+os.makedirs(DISEASE_DIR, exist_ok=True)
+os.makedirs(LOCATION_DIR, exist_ok=True)
+os.makedirs(CELL_TYPE_DIR, exist_ok=True)
+os.makedirs(CELL_LINE_DIR, exist_ok=True)
+os.makedirs(SPECIES_DIR, exist_ok=True)
 
 # Download from https://emmaa.s3.amazonaws.com/assembled/brca/statements_2021-01-30-17-21-54.gz
-DUMMY_EXAMPLE_INDRA = os.path.join(DATA_DIR, 'statements_2021-01-30-17-21-54.json')
+DUMMY_EXAMPLE_INDRA = os.path.join(RAW_DIR, 'statements_2021-01-30-17-21-54.json')
 # Can be created by running python -m src.data.indra
 DUMMY_EXAMPLE_TRIPLES = os.path.join(DATA_DIR, 'organ.tsv')
--- src/data/indra.py
+++ src/data/indra.py
@@ -14,15 +14,27 @@ import pandas as pd
 import pybel
 from pybel.constants import ANNOTATIONS, EVIDENCE, RELATION, CITATION
 
-from ..constants import DATA_DIR, DUMMY_EXAMPLE_INDRA
+from ..constants import (
+    DUMMY_EXAMPLE_INDRA,
+    MISC_DIR,
+    INPUT_DIR,
+    SPECIES_DIR,
+    ORGAN_DIR,
+    CELL_TYPE_DIR,
+    CELL_LINE_DIR,
+    LOCATION_DIR,
+    DISEASE_DIR,
+)
 
 logger = logging.getLogger(__name__)
 
 
-def create_context_subgraph(graph: pybel.BELGraph, context_annotations: List[str]) -> pybel.BELGraph:
+def create_context_type_specific_subgraph(graph: pybel.BELGraph, context_annotations: List[str]) -> pybel.BELGraph:
     """Create a subgraph based on context annotations."""
     subgraph = graph.child()
     subgraph.name = f'INDRA graph contextualized for {context_annotations}'
+
+    edges_to_remove = []
 
     # Iterate through the graph and infer a subgraph with edges that contain the annotation of interest
     for u, v, k, data in graph.edges(data=True, keys=True):
@@ -31,6 +43,16 @@ def create_context_subgraph(graph: pybel.BELGraph, context_annotations: List[str
             for annotation in context_annotations
         ):
             subgraph.add_edge(u, v, k, **data)
+            # Triples to be removed
+            edges_to_remove.append((u, v, k))
+
+    number_of_edges_before = graph.number_of_edges()
+    graph.remove_edges_from(edges_to_remove)
+    number_of_edges_after_removing_annotations = graph.number_of_edges()
+
+    logger.info(
+        f'Original graph was reduced from {number_of_edges_before} to {number_of_edges_after_removing_annotations} edges'
+    )
 
     string = f'Number of nodes/edges in the inferred subgraph "{context_annotations}": \
     {subgraph.number_of_nodes()} {subgraph.number_of_edges()}'
@@ -40,7 +62,12 @@ def create_context_subgraph(graph: pybel.BELGraph, context_annotations: List[str
     return subgraph
 
 
-def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> Dict[str, Any]:
+def dump_edgelist(
+    graph: pybel.BELGraph,
+    annotations: List[str],
+    name: str,
+    output_dir: str,
+) -> Dict[str, Any]:
     """Dump tsv file for ml purposes."""
     triples = []
 
@@ -78,6 +105,14 @@ def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> D
                     },
                 )
 
+    if not triples:
+        return {
+            'context': name,
+            'number_of_triples': '0',
+            'number_of_labels': '0',
+            'labels': '0',
+        }
+
     df = pd.DataFrame(triples)
 
     """Removing labels that appear less than X% from the total"""
@@ -106,7 +141,7 @@ def dump_edgelist(graph: pybel.BELGraph, annotations: List[str], name: str) -> D
     summary['number_of_labels'] = len(final_labels)
     summary['labels'] = df['class'].value_counts().to_dict()
 
-    df.to_csv(os.path.join(DATA_DIR, f'{name}.tsv'), sep='\t', index=False)
+    df.to_csv(os.path.join(output_dir, f'{name}.tsv'), sep='\t', index=False)
 
     return summary
 
@@ -115,8 +150,10 @@ def read_indra_triples(
     path: str = DUMMY_EXAMPLE_INDRA,  # TODO: change
 ):
     """Parse indra statements in JSON and returns context specific graphs."""
+    #: Read file INDRA KG
     indra_kg = pybel.io.indra.from_indra_statements_json_file(DUMMY_EXAMPLE_INDRA)
 
+    #: Summarize content of the KG
     logger.info(indra_kg.summarize())
 
     # Print all the annotations in the graph
@@ -127,21 +164,68 @@ def read_indra_triples(
             for key in data[ANNOTATIONS]:
                 all_annotations.add(key)
 
+    # Summarize all annotations
     logger.info(all_annotations)
 
-    organ_subgraph = create_context_subgraph(indra_kg, ['MeSHAnatomy'])
-    species_subgraph = create_context_subgraph(indra_kg, ['TAX_ID'])
-    disease_subgraph = create_context_subgraph(indra_kg, ['MeSHDisease', 'Disease'])
-    cell_type_subgraph = create_context_subgraph(indra_kg, ['Cell'])
-    cell_line_subgraph = create_context_subgraph(indra_kg, ['CellLine'])
-    location_subgraph = create_context_subgraph(indra_kg, ['CellStructure'])
+    """
+    Split the KG into two big chunks:
 
-    organ_summary = dump_edgelist(organ_subgraph, ['MeSHAnatomy'], 'organ')
-    species_summary = dump_edgelist(species_subgraph, ['TAX_ID'], 'species')
-    disease_summary = dump_edgelist(disease_subgraph, ['MeSHDisease', 'Disease'], 'disease')
-    cell_type_summary = dump_edgelist(cell_type_subgraph, ['Cell'], 'cell_type')
-    cell_line_summary = dump_edgelist(cell_line_subgraph, ['CellLine'], 'cell_line')
-    location_summary = dump_edgelist(location_subgraph, ['CellStructure'], 'location')
+    1. Pre-training dataset -> Any triple + text evidence that is not in any of the 6 annotation types above
+    The rationale is that we want to have a common pre-trained model that can be used as a basis for all the fine tuning
+    classification tasks.
+
+    2. Fine-tuning datasets (benchmark for our models). 6 different train-validation-test splits for each of the 
+    annotation types (each of them will contain multiple classes).
+
+    We would like to note that the pre-training dataset is significantly larger than the fine-tuning dataset.
+    Naturally, MultiSTonKGs requires a pre-training similar to the other two baselines models (i.e., KG-based has been
+    trained based on node2vec on the INDRA KG and BioBERT was trained on PubMed).
+    """
+
+    organ_subgraph = create_context_type_specific_subgraph(indra_kg, ['MeSHAnatomy'])
+    species_subgraph = create_context_type_specific_subgraph(indra_kg, ['TAX_ID'])
+    disease_subgraph = create_context_type_specific_subgraph(indra_kg, ['MeSHDisease', 'Disease'])
+    cell_type_subgraph = create_context_type_specific_subgraph(indra_kg, ['Cell'])
+    cell_line_subgraph = create_context_type_specific_subgraph(indra_kg, ['CellLine'])
+    location_subgraph = create_context_type_specific_subgraph(indra_kg, ['CellStructure'])
+
+    #: Dump the 6 annotation type specific subgraphs (triples)
+    organ_summary = dump_edgelist(
+        graph=organ_subgraph,
+        annotations=['MeSHAnatomy'],
+        name='organ',
+        output_dir=ORGAN_DIR,
+    )
+    species_summary = dump_edgelist(
+        graph=species_subgraph,
+        annotations=['TAX_ID'],
+        name='species',
+        output_dir=SPECIES_DIR,
+    )
+    disease_summary = dump_edgelist(
+        graph=disease_subgraph,
+        annotations=['MeSHDisease', 'Disease'],
+        name='disease',
+        output_dir=DISEASE_DIR,
+    )
+    cell_type_summary = dump_edgelist(
+        graph=cell_type_subgraph,
+        annotations=['Cell'],
+        name='cell_type',
+        output_dir=CELL_TYPE_DIR,
+    )
+    cell_line_summary = dump_edgelist(
+        graph=cell_line_subgraph,
+        annotations=['CellLine'],
+        name='cell_line',
+        output_dir=CELL_LINE_DIR,
+    )
+    location_summary = dump_edgelist(
+        graph=location_subgraph,
+        annotations=['CellStructure'],
+        name='location',
+        output_dir=LOCATION_DIR,
+    )
 
     summary_df = pd.DataFrame([
         organ_summary,
@@ -151,8 +235,29 @@ def read_indra_triples(
         cell_line_summary,
         location_summary,
     ])
+    summary_df.to_csv(os.path.join(MISC_DIR, 'summary.tsv'), sep='\t', index=False)
 
-    summary_df.to_csv(os.path.join(DATA_DIR, 'summary.tsv'), sep='\t', index=False)
+    """Dump pre training dataset."""
+    triples = []
+
+    # Iterate through the graph and infer a subgraph with edges that contain the annotation of interest
+    for u, v, data in indra_kg.edges(data=True):
+        # Skip relations without evidences
+        # TODO: skip specific relations
+        if not EVIDENCE in data:
+            continue
+
+        triples.append({
+            'source': u,
+            'relation': data[RELATION],
+            'target': v,
+            'evidence': data[EVIDENCE],
+            'pmid': data[CITATION],
+        })
+
+    pretraining_triples = pd.DataFrame(triples)
+    del triples
+    pretraining_triples.to_csv(os.path.join(INPUT_DIR, 'pretraining_triples.tsv'), sep='\t', index=False)
 
 
 if __name__ == '__main__':
