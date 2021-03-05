d22171da9d74268089013555091d49806928876a
helena-balabin <helena.balabin@gmail.com>
2021-03-05 11:48:59 +0100

    Set up a simple cv splits in model classes

---
+++ src/models/kg_baseline_model.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""KG baseline model on the fine-tuning classification task, assuming the model embeddings are pre-trained."""
+
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from typing import List
+
+
+def get_train_test_splits(
+    train_data: pd.DataFrame,
+    type_column_name: str = "class",
+    random_seed: int = 42,
+    n_splits: int = 5
+) -> List:
+    """ Returns train/test indices for n_splits many splits based on the fine-tuning dataset that is passed"""
+
+    # Leave out the label in the dataset
+    X = train_data.drop(type_column_name, axis=1)
+    y = train_data[type_column_name]
+
+    # TODO: think about whether a validation split is necessary
+    # For now: implement stratified train/test splits
+    skf = StratifiedKFold(n_splits=n_splits, random_state=random_seed, shuffle=False)
+
+    return [[train_idx, test_idx] for train_idx, test_idx in skf.split(X, y)]
