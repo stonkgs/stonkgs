4faa2da99a504df26a7e3bc6649784de92756eeb
helena-balabin <helena.balabin@gmail.com>
2021-03-12 17:32:32 +0100

    Continue implementing the KG baseline

--- src/models/kg_baseline_model.py
+++ src/models/kg_baseline_model.py
@@ -12,8 +12,8 @@ from typing import Dict, List
 import numpy as np
 import pandas as pd
 import torch
-from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import f1_score
+from sklearn.model_selection import StratifiedKFold
 
 from constants import DUMMY_EXAMPLE_TRIPLES, RANDOM_WALKS_PATH, EMBEDDINGS_PATH
 
@@ -34,40 +34,63 @@ class KGEClassificationModel(torch.nn.Module):
         3) Softmax
         """
         super(KGEClassificationModel, self).__init__()
-        self.pooling = torch.max(dim=1)
+        self.pooling = torch.max
         self.linear = torch.nn.Linear(d_in, num_classes)
-        # TODO: make sure the dim parameters are correct
         self.softmax = torch.nn.Softmax(dim=1)
+        # TODO: add something here?
 
     def forward(self, x):
         """
         The pooling component of this model is not specified as a separate nn Layer, since there are no parameters
         that need to be learned in this specific type of pooling. This layer returns the class probabilities.
         """
-        h_pooled = self.pooling(x)
+        h_pooled = self.pooling(x, dim=1).values
         linear_output = self.linear(h_pooled)
         y_pred = self.softmax(linear_output)
         return y_pred
 
 
-# TODO: Own dataset class
 class INDRAEntityDataset(torch.utils.data.Dataset):
     """Custom Dataset class for INDRA data."""
 
-    def __init__(self, encodings, labels):
+    def __init__(self, embedding_dict, random_walk_dict, sources, targets, labels, max_len=256):
         # Assumes that the labels are numerically encoded
-        # TODO: do all the embedding mappings in here
-        self.encodings = encodings
+        self.max_length = max_len
+        self.sources = sources
+        self.targets = targets
+        self.embedding_dict = embedding_dict
+        self.random_walk_dict = random_walk_dict
+        self.embeddings = self.get_embeddings()
         self.labels = labels
 
     def __getitem__(self, idx):
-        # TODO: change?
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
+        item = torch.tensor(self.embeddings[idx, :, :], dtype=float)
+        labels = torch.tensor(self.labels[idx])
+        return item, labels
 
     def __len__(self):
         return len(self.labels)
+
+    def get_embeddings(self):
+        number_of_triples = len(self.sources)
+        # Get the embedding dimension by accessing a random element
+        embedding_dim = len(next(iter(self.embedding_dict.values())))
+
+        # Initialize the embedding array of dimension n x random_walk_length x embedding_dim
+        embeddings = np.empty((number_of_triples, self.max_length, embedding_dim))
+
+        # 1. Get random walks for sources and targets using random_walk_dict
+        for idx, (source, target) in enumerate(zip(self.sources, self.targets)):
+            random_walk_source = self.random_walk_dict[source]
+            random_walk_target = self.random_walk_dict[target]
+            # 2. Concatenate and shorten the random walks if needed
+            random_walk = [source] + random_walk_source.tolist()[:(self.max_length//2-1)] + \
+                          [target] + random_walk_target.tolist()[:(self.max_length//2-1)]
+            # 3. Get embeddings for each node using embedding_dict stated by its index in each random walk
+            embeds_random_walk = np.stack([self.embedding_dict[node] for node in random_walk], axis=0)
+            embeddings[idx, :, :] = embeds_random_walk
+
+        return embeddings
 
 
 def _prepare_df(embedding_path: str, sep: str = '\t') -> Dict[str, List[str]]:
@@ -108,8 +131,8 @@ def run_kg_baseline_classification_cv(
     triples_path,
     embedding_path,
     random_walks_path,
-    epochs=10,
-    batch_size=16,
+    epochs=100,
+    batch_size=32,
     lr=1e-4
 ) -> Dict:
     """Run KG baseline classification."""
@@ -127,6 +150,14 @@ def run_kg_baseline_classification_cv(
             'class'
         ],
     )
+    # Numerically encode labels
+    unique_tags = set(label for label in triples_df["class"])
+    tag2id = {label: number for number, label in enumerate(unique_tags)}
+    id2tag = {value: key for key, value in tag2id.items()}
+    # Cross entropy loss weights based on the inverse of the class counts
+    # weights = [1/len([i for i in triples_df["class"] if i == id2tag[id_num]) for id_num in range(len(unique_tags))]
+    # print(weights)
+    labels = pd.Series([int(tag2id[label]) for label in triples_df["class"]])
 
     train_test_splits = get_train_test_splits(triples_df)
 
@@ -136,12 +167,19 @@ def run_kg_baseline_classification_cv(
     # Initialize
     f1_scores = []
 
-    # TODO: 4. Train and test the model in a cv setting
+    # Train and test the model in a cv setting
     for indices in train_test_splits:
-        # TODO: change to KG data processing
-        kg_embeds = INDRAEntityDataset()  # TODO: pass the data correctly
+        kg_embeds = INDRAEntityDataset(
+            embeddings_dict,
+            random_walks_dict,
+            triples_df["source"],
+            triples_df["target"],
+            labels
+        )
 
-        # Sample elements randomly from a given list of ids, no replacement.
+        print(triples_df["class"].value_counts())
+
+        # Sample elements randomly from a given list of ids, no replacement
         train_subsampler = torch.utils.data.SubsetRandomSampler(indices["train_idx"])
         test_subsampler = torch.utils.data.SubsetRandomSampler(indices["test_idx"])
 
@@ -155,10 +193,10 @@ def run_kg_baseline_classification_cv(
             batch_size=batch_size,
             sampler=test_subsampler)
 
-        model = KGEClassificationModel(num_classes=triples_df["class"].unique())
+        model = KGEClassificationModel(num_classes=len(triples_df["class"].unique()))
 
-        # TODO: Change to CE loss for multiclass classification
-        criterion = torch.nn.CrossEntropyLoss()
+        # TODO: add weights
+        criterion = torch.nn.CrossEntropyLoss(reduction="mean") # weight=torch.tensor(
         optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
         # Train the model for epoch many epochs
@@ -166,16 +204,17 @@ def run_kg_baseline_classification_cv(
         for epoch in range(epochs):
             # Iterate over the DataLoader for training data
             for train_data in trainloader:
-                # TODO: Change Get inputs
                 train_inputs, train_labels = train_data
                 # Zero the gradients
                 optimizer.zero_grad()
                 # Perform forward pass
-                train_outputs = model(train_inputs)
+                train_outputs = model(train_inputs.float())
+                print(train_outputs)
 
                 # Compute loss
                 loss = criterion(train_outputs, train_labels)
                 # Perform backward pass
+                # TODO: Loss doesn't decrease???
                 loss.backward()
                 # Perform optimization
                 optimizer.step()
@@ -190,14 +229,16 @@ def run_kg_baseline_classification_cv(
             for test_data in testloader:
                 # Get inputs
                 test_inputs, test_labels = test_data
-                all_true_labels.append(test_labels.tolist())
+                all_true_labels = all_true_labels + test_labels.tolist()
                 # Generate outputs
-                test_outputs = model(test_inputs)
+                test_outputs = model(test_inputs.float())
                 # Class probabilities to labels
                 _, predicted_labels = torch.max(test_outputs.data, 1)
-                all_pred_labels.append(predicted_labels.tolist())
+                all_pred_labels = all_pred_labels + predicted_labels.tolist()
 
         # Use macro average for now
+        print(all_pred_labels)
+
         f1_scores.append(f1_score(all_true_labels, all_pred_labels, average="macro"))
 
     logger.info(f'Mean f1-score: {np.mean(f1_scores)}')
