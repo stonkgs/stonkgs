027b3cc3621c0f017b69f446a6df00eed71198f2
helena-balabin <helena.balabin@gmail.com>
2021-04-19 14:14:52 +0200

    Add a (preliminary) pretraining Trainer + logging procedure

--- src/stonkgs/models/stonkgs_model.py
+++ src/stonkgs/models/stonkgs_model.py
@@ -2,18 +2,39 @@
 
 """STonKGs model on the fine-tuning classification task, assuming the model embeddings are pre-trained."""
 
-from typing import List
+import logging
+import os
+from typing import List, Optional
 
+import mlflow
 import pandas as pd
 import torch
 from datasets import Dataset
 from sklearn.model_selection import StratifiedKFold
 from torch import nn
-from transformers import BertConfig, BertForPreTraining, BertModel, BertTokenizer
+from transformers import (
+    BertConfig,
+    BertForPreTraining,
+    BertModel,
+    BertTokenizer,
+    Trainer,
+    TrainingArguments,
+)
 from transformers.models.bert.modeling_bert import BertForPreTrainingOutput, BertLMPredictionHead
+from transformers.trainer_utils import get_last_checkpoint
 
-from stonkgs.constants import EMBEDDINGS_PATH, NLP_MODEL_TYPE, PRETRAINING_PREPROCESSED_DF_PATH
+from stonkgs.constants import (
+    EMBEDDINGS_PATH,
+    MLFLOW_TRACKING_URI,
+    NLP_MODEL_TYPE,
+    PRETRAINING_PREPROCESSED_DF_PATH,
+    STONKGS_PRETRAINING_DIR,
+)
 from stonkgs.models.kg_baseline_model import _prepare_df
+
+# Initialize logger
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 def _load_pre_training_data(
@@ -226,25 +247,85 @@ class STonKGsForPreTraining(BertForPreTraining):
         )
 
 
-if __name__ == "__main__":
-    # Just a simple test to see if the model can be initialized without errors
+def pretrain_stonkgs(
+    batch_size: int = 8,
+    lr: float = 1e-4,
+    logging_dir: Optional[str] = MLFLOW_TRACKING_URI,
+    logging_steps: int = 100,
+    max_steps: int = 10000,
+    overwrite_output_dir: bool = False,
+    save_limit: int = 5,
+    save_steps: int = 5000,
+    training_dir: str = STONKGS_PRETRAINING_DIR,
+):
+    """Run the pre-training procedure for the STonKGs model based on the transformers Trainer and TrainingArguments."""
+    # Part of this code is taken from
+    # https://github.com/huggingface/transformers/blob/master/examples/language-modeling/run_mlm.py
+
+    # Logging with mlflow
+    # End previous run
+    mlflow.end_run()
+    # Initialize mlflow run, set tracking URI to use the same experiment for all runs,
+    # so that one can compare them
+    mlflow.set_tracking_uri(logging_dir)
+    mlflow.set_experiment('STonKGs Pre-Training')
+
+    # Initialize the STonKGs model
     kg_embed_dict = _prepare_df(EMBEDDINGS_PATH)
-    stonkgs_dummy_model = STonKGsForPreTraining(NLP_MODEL_TYPE, kg_embed_dict)
+    stonkgs_model = STonKGsForPreTraining(NLP_MODEL_TYPE, kg_embed_dict)
 
-    # Load the data and get a first example
-    pretraining_data = _load_pre_training_data()
-    # Put it into a data loader
-    pretraining_dataloader = torch.utils.data.DataLoader(pretraining_data, batch_size=4)
-
-    # Get a batch as an example
-    example = next(iter(pretraining_dataloader))
-
-    # Simple test of a forward pass of one batch
-    stonkgs_dummy_model(
-        input_ids=example['input_ids'],
-        attention_mask=example['attention_mask'],
-        token_type_ids=example['token_type_ids'],
-        masked_lm_labels=example['masked_lm_labels'],
-        ent_masked_lm_labels=example['ent_masked_lm_labels'],
-        next_sentence_labels=example['next_sentence_labels'],
+    # Initialize the TrainingArguments
+    training_args = TrainingArguments(
+        output_dir=training_dir,
+        overwrite_output_dir=overwrite_output_dir,
+        do_train=True,
+        per_device_train_batch_size=batch_size,
+        max_steps=max_steps,  # Use max_steps rather than num_training_epochs
+        learning_rate=lr,  # Default is to use that lr with a linear scheduler
+        logging_dir=logging_dir,
+        logging_steps=logging_steps,
+        save_steps=save_steps,
+        save_total_limit=save_limit,
+        report_to=['mlflow'],
     )
+
+    # Detecting last checkpoint
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. ",
+                "Use --overwrite_output_dir to overcome.",
+            )
+        elif last_checkpoint is not None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change ",
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch.",
+            )
+
+    # Initialize the dataset
+    pretraining_data = _load_pre_training_data()
+
+    # Initialize the Trainer
+    trainer = Trainer(
+        model=stonkgs_model,
+        args=training_args,
+        train_dataset=pretraining_data,
+    )
+    # And train STonKGs to the moon
+    train_result = trainer.train(resume_from_checkpoint=last_checkpoint)
+    trainer.save_model()  # Saves the tokenizer too for easy upload
+    metrics = train_result.metrics
+
+    # Log the number of pre-training samples
+    metrics["train_samples"] = len(pretraining_data)
+    # Log all metrics
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+
+
+if __name__ == "__main__":
+    # TODO: divide this class into three classes: stonkgs_model, stonkgs_pretraining, stonkgs_finetuning
+    pretrain_stonkgs(overwrite_output_dir=True)
