756b2601c3c34c67a7b39960f07bd792b309a1d0
helena-balabin <helena.balabin@gmail.com>
2021-05-04 11:23:47 +0200

    Filter out minority classes in the fine-tuning script rather than the preprocessing part

--- src/stonkgs/models/stonkgs_finetuning.py
+++ src/stonkgs/models/stonkgs_finetuning.py
@@ -2,10 +2,36 @@
 
 """STonKGs model on the fine-tuning classification task, assuming the model embeddings are pre-trained."""
 
+import logging
 from typing import List
 
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def filter_for_majority_classes(
+    df: pd.DataFrame,
+    n_classes: int = 5,
+) -> pd.DataFrame:
+    """Filter out data entries that occur infrequently, return the dataframe with only n_class many majority classes."""
+    # Remove any classes that are not in the n_classes most common classes
+    # (value counts returns classes in descending order)
+    labels_to_remove = df['class'].value_counts()[n_classes:].to_dict()
+
+    # Some statistics
+    logger.info(f'labels removed due to low occurrence {labels_to_remove}')
+    logger.info(f'raw triples {df.shape[0]}')
+
+    # Remove all the entries that are labelled with a class that should be removed
+    df = df[~df['class'].isin(list(labels_to_remove.keys()))]
+
+    # Final length of the filtered df
+    logger.info(f'triples after filtering {df.shape[0]}')
+
+    return df
 
 
 def get_train_test_splits(
