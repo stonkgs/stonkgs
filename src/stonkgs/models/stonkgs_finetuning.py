99eb6ef67a21be1bfffe4fa5b68b20611fa68810
helena-balabin <helena.balabin@gmail.com>
2021-05-30 11:38:18 +0200

    Add a preprocessing routine for the fine-tuning datasets

--- src/stonkgs/models/stonkgs_finetuning.py
+++ src/stonkgs/models/stonkgs_finetuning.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 
-"""STonKGs model on the fine-tuning classification task, assuming the model embeddings are pre-trained."""
+"""Runs the STonKGs model on the fine-tuning classification task, assuming the model embeddings are pre-trained.
+
+Run with:
+python -m src.stonkgs.models.stonkgs_finetuning
+"""
 
 import logging
+import os
 from typing import List
 
 import pandas as pd
 import torch
 from sklearn.model_selection import StratifiedKFold
-from transformers.configuration_utils import PretrainedConfig
 from transformers.modeling_outputs import SequenceClassifierOutput
-from transformers.models.bert import BertModel
+from tqdm import tqdm
+from transformers.models.bert import BertModel, BertTokenizer, BertTokenizerFast
 
-from stonkgs.constants import PRETRAINED_STONKGS_DUMMY_PATH
+from stonkgs.constants import (
+    EMBEDDINGS_PATH,
+    NLP_MODEL_TYPE,
+    ORGAN_DIR,
+    RANDOM_WALKS_PATH,
+    PRETRAINED_STONKGS_DUMMY_PATH,
+    VOCAB_FILE,
+)
+from stonkgs.models.kg_baseline_model import _prepare_df
 from stonkgs.models.stonkgs_model import STonKGsForPreTraining
 
 logger = logging.getLogger(__name__)
@@ -38,12 +51,93 @@ def get_train_test_splits(
 
 def preprocess_fine_tuning_data(
     train_data_path: str,
+    embedding_name_to_vector_path: str = EMBEDDINGS_PATH,
+    embedding_name_to_random_walk_path: str = RANDOM_WALKS_PATH,
+    nlp_model_type: str = NLP_MODEL_TYPE,
+    sep_id: int = 102,
+    unk_id: int = 100,
 ) -> pd.DataFrame:
     """Generates input_ids, attention_mask, token_type_ids etc. based on the source, target, evidence columns."""
-    unprocessed_df = pd.read_csv(train_data_path, sep='\t')
+    # Load the KG embedding dict to convert the names to numeric indices
+    kg_embed_dict = _prepare_df(embedding_name_to_vector_path)
+    kg_name_to_idx = {key: i for i, key in enumerate(kg_embed_dict.keys())}
+    # Load the random walks for each node
+    random_walk_dict = _prepare_df(embedding_name_to_random_walk_path)
+    # Convert random walk sequences to list of numeric indices
+    random_walk_idx_dict = {k: [kg_name_to_idx[node] for node in v] for k, v in random_walk_dict.items()}
+
+    # Load the raw fine-tuning dataset with source, target and evidence
+    unprocessed_df = pd.read_csv(train_data_path, sep='\t', usecols=["source", "target", "evidence", "class"])
+
+    # Check how many nodes in the fine-tuning dataset are not covered by the learned KG embeddings
+    number_of_pre_training_nodes = len(set(unprocessed_df["source"]).union(set(unprocessed_df["target"])))
+    if number_of_pre_training_nodes > len(kg_embed_dict):
+        logger.warning(f'{number_of_pre_training_nodes - len(kg_embed_dict)} out of {number_of_pre_training_nodes}'
+                       f'nodes are not covered by the embeddings learned in the pretraining dataset')
+
+    # Get the length of the text or entity embedding sequences (2 random walks + 2 = entity embedding sequence length)
+    random_walk_length = len(next(iter(random_walk_idx_dict.values())))
+    half_length = random_walk_length * 2 + 2
+
+    # Initialize a FAST tokenizer if it's the default one (BioBERT)
+    if nlp_model_type == NLP_MODEL_TYPE:
+        # Initialize the fast tokenizer for getting the text token ids
+        tokenizer = BertTokenizerFast(vocab_file=VOCAB_FILE)
+    else:
+        # Initialize a slow tokenizer used for getting the text token ids
+        tokenizer = BertTokenizer.from_pretrained(nlp_model_type)
+
+    # Initialize the preprocessed data
+    fine_tuning_preprocessed = []
+
+    # Log progress with a progress bar
+    for idx, row in tqdm(
+        unprocessed_df.iterrows(),
+        total=unprocessed_df.shape[0],
+        desc='Preprocessing the fine-tuning dataset',
+    ):
+        # 1. "Token type IDs": 0 for text tokens, 1 for entity tokens
+        token_type_ids = [0] * half_length + [1] * half_length
+
+        # 2. Tokenization for getting the input ids and attention masks for the text
+        # Use encode_plus to also get the attention mask ("padding" mask)
+        encoded_text = tokenizer.encode_plus(
+            row['evidence'],
+            padding='max_length',
+            truncation=True,
+            max_length=half_length,
+        )
+        text_token_ids = encoded_text['input_ids']
+        text_attention_mask = encoded_text['attention_mask']
+
+        # 3. Get the random walks sequence and the node indices, add the SEP (usually with id=102) in between
+        # Use a sequence of UNK tokens if the node is not contained in the dictionary of the nodes from pre-training
+        random_w_source = random_walk_idx_dict[row['source']] if row['source'] in random_walk_idx_dict.keys() \
+            else [unk_id] * random_walk_length
+        random_w_target = random_walk_idx_dict[row['target']] if row['target'] in random_walk_idx_dict.keys() \
+            else [unk_id] * random_walk_length
+        random_w_ids = random_w_source + [sep_id] + random_w_target + [sep_id]
+
+        # 4. Total attention mask (attention mask is all 1 for the entity sequence)
+        attention_mask = text_attention_mask + [1] * half_length
+
+        # 5. Total input_ids = half text ids + half entity ids
+        input_ids = text_token_ids + random_w_ids
+
+        # Add all the features to the preprocessed data
+        fine_tuning_preprocessed.append({
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'token_type_ids': token_type_ids,  # Remove the MLM, ELM and NSP labels since it's not needed anymore
+            'label': row['class'],  # Add the annotation/relation label for fine-tuning instead
+        })
+
+    # Put the preprocessed data into a dataframe
+    fine_tuning_preprocessed_df = pd.DataFrame(fine_tuning_preprocessed)
+
+    return fine_tuning_preprocessed_df
 
 
-# TODO: fine-tuning class
 class STonKGsForSequenceClassification(STonKGsForPreTraining):
     """Create the fine-tuning part of the STonKGs model based the pre-trained STonKGs model.
 
@@ -57,13 +151,12 @@ class STonKGsForSequenceClassification(STonKGsForPreTraining):
         self.num_labels = config.num_labels
         self.config = config
 
-        # TODO put from pretrained here? or is this happening automatically?
         # Load the pretrained STonKGs Transformer here
         self.bert = BertModel(config)
-        # TODO: implement dropout in the KG baseline model, too?
         self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
         self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
 
+        # Initialize all the pre-trained as well as new weights (i.e. classifier weights) here
         self.init_weights()
 
     def forward(
@@ -149,10 +242,12 @@ class STonKGsForSequenceClassification(STonKGsForPreTraining):
 
 if __name__ == "__main__":
     # Add a test for loading the model
-    unique_tags = 10
-    # config = PretrainedConfig.from_pretrained(PRETRAINED_STONKGS_DUMMY_PATH)
-    model = STonKGsForSequenceClassification.from_pretrained(
-        pretrained_model_name_or_path=PRETRAINED_STONKGS_DUMMY_PATH,
-        num_labels=unique_tags)
+    # unique_tags = 10
+    # model = STonKGsForSequenceClassification.from_pretrained(
+    #     pretrained_model_name_or_path=PRETRAINED_STONKGS_DUMMY_PATH,
+    #     num_labels=unique_tags)
     # TODO: set up fine tuning
 
+    # Add a test for loading and preprocessing the fine-tuning data
+    test_df = preprocess_fine_tuning_data(os.path.join(ORGAN_DIR, 'organ_filtered.tsv'))
+    logger.info(test_df.head())
