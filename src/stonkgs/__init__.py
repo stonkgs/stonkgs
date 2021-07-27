2c7d10c563d703b3ed0a434985857ba0e313697d
helena-balabin <helena.balabin@gmail.com>
2021-07-27 13:44:00 +0200

    Add simpler imports for embedding extraction functions

--- src/stonkgs/__init__.py
+++ src/stonkgs/__init__.py
@@ -3,9 +3,12 @@
 """STonKGs."""
 
 from .models.stonkgs_finetuning import STonKGsForSequenceClassification
+from .models.stonkgs_for_embeddings import get_stonkgs_embeddings, preprocess_file_for_embeddings
 from .models.stonkgs_model import STonKGsForPreTraining
 
 __all__ = [
+    "get_stonkgs_embeddings",
+    "preprocess_file_for_embeddings",
     "STonKGsForPreTraining",
     "STonKGsForSequenceClassification",
 ]
