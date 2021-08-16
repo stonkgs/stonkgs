5dfecdeff526fe8837d9c34318f87e4ad07988aa
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-16 16:32:57 +0200

    Add remaining fine tuned tasks from zenodo

--- src/stonkgs/api/constants.py
+++ src/stonkgs/api/constants.py
@@ -9,10 +9,11 @@ import pystow
 STONKGS = pystow.module("stonkgs")
 
 SPECIES_RECORD = "5205530"
-SPECIES_CONFIG = "https://zenodo.org/record/5205530/files/config.json"
-SPECIES_BIN = "https://zenodo.org/record/5205530/files/pytorch_model.bin"
-SPECIES_TRAINING = "https://zenodo.org/record/5205530/files/training_args.bin"
-SPECIES_MODULE = STONKGS.submodule("species")
+LOCATION_RECORD = "5205553"
+DISEASE_RECORD = "5205592"
+CORRECT_MULTICLASS_RECORD = "5206139"
+CORRECT_BINARY_RECORD = "5205989"
+CELL_LINE_RECORD = "5205915"
 
 WALKS_URL = "https://zenodo.org/record/5205687/files/random_walks_best_model.tsv"
 EMBEDDINGS_URL = "https://zenodo.org/record/5205687/files/embeddings_best_model.tsv"
@@ -28,6 +29,12 @@ def ensure_embeddings() -> Path:
     return STONKGS.ensure(url=EMBEDDINGS_URL)
 
 
+def _ensure_fine_tuned(submodule, record) -> Path:
+    STONKGS.ensure(submodule, url=f"https://zenodo.org/record/{record}/files/config.json")
+    STONKGS.ensure(submodule, url=f"https://zenodo.org/record/{record}/files/training_args.bin")
+    return STONKGS.ensure(submodule, url=f"https://zenodo.org/record/{record}/files/pytorch_model.bin")
+
+
 def ensure_species() -> Path:
     """Ensure that the species model is downloaded from `Zenodo <https://zenodo.org/record/5205530>`_.
 
@@ -35,6 +42,29 @@ def ensure_species() -> Path:
 
     .. warning:: It's pretty big - 1.4GB. Be patient!
     """
-    SPECIES_MODULE.ensure(url=SPECIES_CONFIG)
-    SPECIES_MODULE.ensure(url=SPECIES_TRAINING)
-    return SPECIES_MODULE.ensure(url=SPECIES_BIN)
+    return _ensure_fine_tuned("species", SPECIES_RECORD)
+
+
+def ensure_location() -> Path:
+    """Ensure that the location model is downloaded."""
+    return _ensure_fine_tuned("location", LOCATION_RECORD)
+
+
+def ensure_disease() -> Path:
+    """Ensure that the disease model is downloaded."""
+    return _ensure_fine_tuned("disease", DISEASE_RECORD)
+
+
+def ensure_correct_multiclass() -> Path:
+    """Ensure that the correct (multiclass) model is downloaded."""
+    return _ensure_fine_tuned("correct_multiclass", CORRECT_MULTICLASS_RECORD)
+
+
+def ensure_correct_binary() -> Path:
+    """Ensure that the correct (binary) model is downloaded."""
+    return _ensure_fine_tuned("correct_binary", CORRECT_BINARY_RECORD)
+
+
+def ensure_cell_binary() -> Path:
+    """Ensure that the cell line model is downloaded."""
+    return _ensure_fine_tuned("cell_line", CELL_LINE_RECORD)
