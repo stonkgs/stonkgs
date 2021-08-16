0905dd3cb32f00d220c676f2ea2acef670173da5
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-16 14:54:38 +0200

    Bump version: 0.1.1-dev → 0.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f'{date.today().year}, helena balabin'
 author = 'helena balabin'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.1.1-dev'
+release = '0.1.1'
 
 # The short X.Y version.
 parsed_version = re.match(
