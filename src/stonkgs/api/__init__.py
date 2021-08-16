660920d39148ba5911cd6955041cb215a8e0b301
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-16 11:57:44 +0200

    Begin adding user code

---
+++ src/stonkgs/api/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+"""Code for downstream users to take advantage of the pre-trained and fine-tuned models."""
---
+++ src/stonkgs/api/datasets.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""Functionality for ensuring the fine-tuned models are ready to use."""
+
+from pathlib import Path
+
+import pystow
+
+STONKGS = pystow.module('stonkgs')
+
+SPECIES_RECORD = '5205530'
+SPECIES_CONFIG = 'https://zenodo.org/record/5205530/files/config.json'
+SPECIES_BIN = 'https://zenodo.org/record/5205530/files/pytorch_model.bin'
+SPECIES_TRAINING = 'https://zenodo.org/record/5205530/files/training_args.bin'
+SPECIES_MODULE = STONKGS.submodule('species')
+
+
+def ensure_species() -> Path:
+    """Ensure that the species model is downloaded from `Zenodo <https://zenodo.org/record/5205530>`_.
+
+    :returns: The path to the model binary
+
+    .. warning:: It's pretty big - 1.4GB. Be patient!
+    """
+    SPECIES_MODULE.ensure(url=SPECIES_CONFIG)
+    SPECIES_MODULE.ensure(url=SPECIES_TRAINING)
+    return SPECIES_MODULE.ensure(url=SPECIES_BIN)
