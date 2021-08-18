fd0a4a4a4b1bdcfb064aad65c708dcda728c5ddd
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-18 10:39:48 +0200

    Stream, yo

--- src/stonkgs/api/get_emmaa.py
+++ src/stonkgs/api/get_emmaa.py
@@ -2,8 +2,10 @@
 
 """Demo of assessing a full EMMAA model."""
 
+import csv
 import gzip
 import json
+import logging
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,26 +15,32 @@ from indra.statements import stmts_from_json
 
 import stonkgs
 
-URL = "https://emmaa.s3.amazonaws.com/assembled/covid19/statements_2021-08-16-20-29-07.gz"
-STATEMENTS_PATH = pystow.ensure("stonkgs", "demos", "emmaa-covid", url=URL)
+URL = "https://emmaa.s3.amazonaws.com/assembled/marm_model/statements_2021-08-17-17-31-53.gz"
+# URL = "https://emmaa.s3.amazonaws.com/assembled/covid19/statements_2021-08-16-20-29-07.gz"
+# URL = "https://emmaa.s3.amazonaws.com/assembled/nf/statements_2021-08-16-18-37-34.gz"
+STATEMENTS_PATH = pystow.ensure("stonkgs", "demos", "emmaa", URL.split("/")[-2], url=URL)
 RESULTS_PATH = STATEMENTS_PATH.with_suffix(".results.tsv")
 SCATTER_PATH = STATEMENTS_PATH.with_suffix(".scatter.svg")
 
 
 def main():
     """Run the EMMAA demo."""
-    if RESULTS_PATH.is_file():
+    if RESULTS_PATH.is_file() and False:
         df = pd.read_csv(RESULTS_PATH, sep="\t")
     else:
         with gzip.open(STATEMENTS_PATH, "rt") as file:
             statements = stmts_from_json(json.load(file))
-        df = stonkgs.infer_correct_binary(statements)
-        df.to_csv(RESULTS_PATH, sep="\t", index=False)
+        with RESULTS_PATH.open("w") as file:
+            writer = csv.writer(file, delimiter="\t")
+            # TODO header
+            writer.writerows(stonkgs.infer_correct_binary(statements))
 
+    return
     fig, ax = plt.subplots(1, 1)
     sns.scatterplot(data=df, x="correct", y="belief", ax=ax)
     fig.savefig(SCATTER_PATH)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     main()
