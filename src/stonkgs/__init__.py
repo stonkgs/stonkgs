0802b8d4deb4a2a17d26062366ffee29a0555fe4
helena-balabin <helena.balabin@gmail.com>
2021-10-13 17:25:23 +0200

    Add ProtSTonKGs to init

--- src/stonkgs/__init__.py
+++ src/stonkgs/__init__.py
@@ -11,6 +11,8 @@ from .api.api import (
     infer_locations,
     infer_species,
 )
+from .models.protstonkgs_model import ProtSTonKGsForPreTraining
+from .models.protstonkgs_finetuning import ProtSTonKGsForSequenceClassification
 from .models.stonkgs_finetuning import STonKGsForSequenceClassification
 from .models.stonkgs_for_embeddings import get_stonkgs_embeddings, preprocess_df_for_embeddings
 from .models.stonkgs_model import STonKGsForPreTraining
@@ -18,6 +20,8 @@ from .models.stonkgs_model import STonKGsForPreTraining
 __all__ = [
     "get_stonkgs_embeddings",
     "preprocess_df_for_embeddings",
+    "ProtSTonKGsForSequenceClassification",
+    "ProtSTonKGsForPreTraining",
     "STonKGsForPreTraining",
     "STonKGsForSequenceClassification",
     "infer_cell_lines",
