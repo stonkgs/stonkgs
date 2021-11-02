6fd093f45ec113caf8d2d546301937c06530ed50
helena.balabin <helena.balabin@scai.fraunhofer.de>
2021-11-02 12:06:04 +0100

    Generalize the broken pre-training file script for fixing other datasets as well

--- src/stonkgs/data/fix_broken_pretraining_dataset.py
+++ src/stonkgs/data/fix_broken_pretraining_dataset.py
@@ -6,6 +6,8 @@ Run with:
 python -m src.stonkgs.data.fix_broken_pretraining_dataset
 """
 import os
+
+import click
 import pandas as pd
 from tqdm import tqdm
 
@@ -14,14 +16,33 @@ from stonkgs.constants import (
 )
 
 
-def convert_tsv_with_str_list(
-    file_name: str,
-    path: str = PRETRAINING_DIR,
+@click.command()
+@click.option(
+    "--chunk_size",
+    default=100000,
+    help="Size of the chunks used for processing the corrupted file",
+    type=int,
+)
+@click.option(
+    "--input_path",
+    default=os.path.join(PRETRAINING_DIR, "pretraining_transe_preprocessed.tsv"),
+    help="Path to the corrupted file (a .tsv file)",
+    type=str,
+)
+@click.option(
+    "--output_path",
+    default=os.path.join(PRETRAINING_DIR, "pretraining_transe_preprocessed_fixed.pkl"),
+    help="Output file path specification (ending in .pkl)",
+    type=str,
+)
+def convert_tsv_to_pkl(
+    chunk_size: int,
+    input_path: str,
+    output_path: str,
 ):
     """Converts strings that look like lists into actual lists of ints and saves a pickle of the repaired dataframe."""
-    converter = lambda x: [int(y) for y in x.strip("[]").split(", ")]
     converter_dict = {
-        col: converter
+        col: lambda x: [int(y) for y in x.strip("[]").split(", ")]
         for col in [
             "input_ids",
             "attention_mask",
@@ -30,25 +51,25 @@ def convert_tsv_with_str_list(
             "ent_masked_lm_labels",
         ]
     }
+    converter_dict["next_sentence_labels"] = lambda x: int(x)  # type: ignore  # noqa
     chunks = []
 
     # Process dataframe in bits with a progress bar
     for chunk in tqdm(
         pd.read_csv(
-            os.path.join(path, file_name + ".tsv"),
+            input_path,
             sep="\t",
-            chunksize=10000,
+            chunksize=chunk_size,
             converters=converter_dict,
-        ),
-        total=10,  # Hard coded total number
+        )
     ):
         chunks.append(chunk)
     complete_df = pd.concat(chunks, axis=0)
 
     # Pickle the complete dataframe
-    complete_df.to_pickle(os.path.join(PRETRAINING_DIR, file_name + ".pkl"))
+    complete_df.to_pickle(output_path)
 
 
 if __name__ == "__main__":
-    # Fixing the positive dataset
-    convert_tsv_with_str_list("pretraining_preprocessed_dummy")
+    # Fixing the dataset
+    convert_tsv_to_pkl()
